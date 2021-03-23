c75ed0009130cf9a50d39306d77ea32480e08f88
Bastian Hodapp <bastianhodapp@gmail.com>
2021-03-23 19:54:12 +0100

    Fix task 2
    parse_int can now deal with strings starting with digits

--- Aufgaben/02/main.cpp
+++ Aufgaben/02/main.cpp
@@ -8,14 +8,13 @@
 
 namespace aufgabe_02 {
     bool is_digit(char c) {
-        int asciiValue = (int) c;
-        return asciiValue >= 48 && asciiValue <= 57;
+        return c >= '0' && c <= '9';
     }
 
     bool is_sign(char c, int *sign) { // * -> Nimmt pointer als Argument
-        if (c == 43) {
+        if (c == '+') {
             *sign = 1;
-        } else if (c == 45) {
+        } else if (c == '-') {
             *sign = -1;
         } else {
             return false;
@@ -24,26 +23,26 @@ namespace aufgabe_02 {
     }
 
     int parse_int(const std::string &text) {
-        int signMultiplicator;
+        int signMultiplicator = 1;
 
-        if (!aufgabe_02::is_sign(text[0], &signMultiplicator)) {
-            throw std::invalid_argument("First char is not a sign");
+        if (!(aufgabe_02::is_sign(text[0], &signMultiplicator) || aufgabe_02::is_digit(text[0]))) {
+            throw std::invalid_argument("First char is not a sign or a digit");
         };
 
         int solution = 0;
-        for (int i = text.length() - 1; i >= 1; i--) {
+        for (int i = text.length() - 1; i >= (aufgabe_02::is_digit(text[0]) ? 0 : 1); i--) {
             if (!is_digit(text[i])) {
                 throw std::invalid_argument("Char at pos " + std::to_string(i) + " is not a digit");
             }
-            int numberValue = text[i] - 48; // 48 -> ASCII-Null (0)
+            int numberValue = text[i] - '0'; // 48 -> ASCII-Null (0)
             solution += numberValue * pow(10, text.length() - i - 1);
         }
 
         return solution * signMultiplicator;
     }
 
-    void parse_int_pointer(const std::string &text, int *solution) {
-        *solution = parse_int(text);
+    int parse_int_pointer(const std::string *text) {
+        return(parse_int(*text));
     }
 }
 
@@ -68,19 +67,14 @@ void parse_int_test() {
     assert(aufgabe_02::parse_int("+0") == 0);
     assert(aufgabe_02::parse_int("-0") == 0);
     try {
-        assert(aufgabe_02::parse_int("9001") == 9001); //No sign
-        assert(false);
-    } catch (...) {}
-    try {
         assert(aufgabe_02::parse_int("+13/2") == 345326); //Non-digit in number
         assert(false);
     } catch (...) {}
 }
 
 void parse_int_pointer_test() {
-    int solution;
-    aufgabe_02::parse_int_pointer("+1232", &solution);
-    assert(solution == 1232);
+    std::string test_string = "+1232";
+    assert(aufgabe_02::parse_int_pointer(&test_string) == 1232);
 }
 
 int main() {
