533f0a307b93400941990e97c80a36e920433887
Bastian Hodapp <bastianhodapp@gmail.com>
2021-03-30 14:10:11 +0200

    Add points distance function

--- 02_klassen_und_objekte/main.cpp
+++ 02_klassen_und_objekte/main.cpp
@@ -3,10 +3,15 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 class Point {
 private:
     int x, y;
+
+    int squared_delta(int v, int w) {
+        return pow(v - w, 2);
+    }
 
 public:
     int getX() {
@@ -24,6 +29,12 @@ public:
     void setY(int y) {
         this->y = y;
     }
+
+    double distance(Point &other) {
+        double squared_delta_x = squared_delta(other.getX(), getX());
+        double squared_delta_y = squared_delta(other.getY(), getY());
+        return sqrt(squared_delta_x + squared_delta_y);
+    }
 };
 
 int main() {
@@ -31,10 +42,22 @@ int main() {
 
     Point point;
 
+    //Simple field values
+    std::cout << "--- simple field values ---" << std::endl;
     point.setX(3);
+    point.setY(5);
     int x = point.getX();
 
     std::cout << x << std::endl;
+
+    //Distance
+    std::cout << "--- distance ---" << std::endl;
+    Point other_point;
+    other_point.setX(10);
+    other_point.setY(-4);
+
+
+    std::cout << point.distance(other_point) << std::endl;
 
     std::cout << "Terminating..." << std::endl;
 
