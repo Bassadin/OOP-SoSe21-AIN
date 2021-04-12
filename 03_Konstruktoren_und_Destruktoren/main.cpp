4ba5b0f1c1e117e272f61377324311bb11b8257f
Bastian Hodapp <bastianhodapp@gmail.com>
2021-04-12 16:41:56 +0200

    Starting chapter 03 work

---
+++ 03_Konstruktoren_und_Destruktoren/main.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <iostream>
+
+class Point
+{
+private:
+    int x, y;
+
+    int squared_delta(int v, int w)
+    {
+        return pow(v - w, 2);
+    }
+
+public:
+    int getX()
+    {
+        return x;
+    }
+
+    void setX(int x)
+    {
+        this->x = x;
+    }
+
+    int getY()
+    {
+        return y;
+    }
+
+    void setY(int y)
+    {
+        this->y = y;
+    }
+
+    double distance(Point &other)
+    {
+        double squared_delta_x = squared_delta(other.getX(), getX());
+        double squared_delta_y = squared_delta(other.getY(), getY());
+        return sqrt(squared_delta_x + squared_delta_y);
+    }
+};
+
+int main()
+{
+    std::cout << "starting..." << std::endl;
+}
