b4080c4a67ce4108468bd0f4434a64242008257e
Bastian Hodapp <bastianhodapp@gmail.com>
2021-05-11 14:44:25 +0200

    start task 8

---
+++ Aufgaben/08/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by basti on 11.05.2021.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "starting..." << std::endl;
+
+    std::cout << "terminating..." << std::endl;
+    return 0;
+}
