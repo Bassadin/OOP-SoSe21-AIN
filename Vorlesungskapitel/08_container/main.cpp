601048a02777bbd0628f2d17938a6004ba37ab9e
Bastian Hodapp <bastianhodapp@gmail.com>
2021-05-18 15:23:38 +0200

    more lesson 8 stuff

--- Vorlesungskapitel/08_container/main.cpp
+++ Vorlesungskapitel/08_container/main.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 #include <array>
+#include <vector>
+#include <set>
 
 namespace container_08 {
     class Point {
@@ -29,16 +31,65 @@ namespace container_08 {
             return os;
         }
     };
+
+    void arrayExample() {
+        std::cout << "-------------------" << std::endl;
+        std::cout << "std::array example" << std::endl;
+
+        std::array<container_08::Point, 2> points = {container_08::Point(47,11), container_08::Point(23,42)};
+        std::cout << points[0] << std::endl; //Ganz üblicher Zugriff
+        std::cout << points[3] << std::endl; //Man kann immer noch out of bounds gehen...
+        //std::cout << points.at(3) << std::endl; //Außer mit at() !
+        std::cout << points.size() << std::endl;
+
+        auto morePoints = points;
+        points[0] = container_08::Point(0 ,0);
+        std::cout << morePoints[0] << std::endl; //Keine Veränderung -> Deep copy!
+
+        std::cout << "-------------------" << std::endl;
+    }
+
+    void vectorExample() {
+        std::cout << "-------------------" << std::endl;
+        std::cout << "std::vector example" << std::endl;
+
+        std::vector<container_08::Point> points;
+
+        for (int i = 0; i < 5; ++i) {
+            points.push_back(Point(i,i));
+            std::cout << "Size: " << points.size() << ", Capacity: " << points.capacity() << std::endl;
+        }
+
+        points.push_back(Point(47, 11));
+        std::cout << points[0] << std::endl;
+        std::cout << points.size() << std::endl;
+
+        std::cout << "-------------------" << std::endl;
+    }
+
+    void setExample() {
+        std::cout << "-------------------" << std::endl;
+        std::cout << "std::set example" << std::endl;
+
+        std::set<int> numbers;
+        for (int i = 10; i != 1; i--) {
+            numbers.insert(i);
+        }
+        std::cout << numbers.size() << std::endl;
+
+        std::set<Point> points;
+        points.insert(Point(47, 11));
+
+        std::cout << "-------------------" << std::endl;
+    }
 }
 
 int main() {
     std::cout << "starting..." << std::endl;
 
-    std::array<container_08::Point, 2> points;
-    std::cout << points[0] << std::endl; //Ganz üblicher Zugriff
-    std::cout << points[3] << std::endl; //Man kann immer noch out of bounds gehen...
-    //std::cout << points.at(3) << std::endl; //Außer mit at() !
-    std::cout << points.size() << std::endl;
+    container_08::arrayExample();
+    container_08::vectorExample();
+    container_08::setExample();
 
     std::cout << "terminating..." << std::endl;
     return 0;
